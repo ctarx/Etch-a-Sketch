23b1dd85fb35ded3f540e95acd024a4e2669e07c
ctarx <ctarx@protonmail.com>
2024-10-19 22:16:20 +0100

    Create a webpage with a 16x16 grid of square divs using js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const container = document.querySelector(".container");
+
+for (let i = 1; i <= 256; i++) {
+  const div = document.createElement("div");
+  div.classList.add("grid");
+  container.appendChild(div);
+  div.innerText = `${i}`;
+}
