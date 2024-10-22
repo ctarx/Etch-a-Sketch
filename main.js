1ae1882cbbe399d916ad3081dd1cf4ba791ff19d
ctarx <ctarx@protonmail.com>
2024-10-22 18:47:40 +0100

    Refactor event listeners to use a single listener per cell

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const MIN_SIZE = 1;
+const MAX_SIZE = 100;
 const DEFAULT_SIZE = 16; // Default grid size 16 x 16
 
 const container = document.querySelector(".container");
@@ -5,6 +7,7 @@ const container = document.querySelector(".container");
 // Create buttons variables
 const gridButton = document.querySelector(".grid");
 const drawButton = document.querySelector(".draw");
+const colorButton = document.querySelector(".rnd-color");
 const eraseButton = document.querySelector(".erase");
 const clearButton = document.querySelector(".clear");
 const sizeButton = document.querySelector(".size");
@@ -32,10 +35,60 @@ function resetGrid() {
   container.innerHTML = "";
 }
 
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
 // Function to add listeners to cells
 function addListenersToCells() {
   const cells = document.querySelectorAll(".cell");
   let isDrawing = false;
+  let isDrawingColor = false;
+
+  // Change cell color based on drawing mode.
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", () => {
+      if (isDrawing) {
+        cell.classList.add("color");
+      } else if (isDrawingColor) {
+        const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+        cell.style.backgroundColor = rndCol;
+      } else {
+        cell.style.backgroundColor = ""; // For erase
+        cell.classList.remove("color");
+      }
+    });
+  });
+
+  // Define functions that set the mode.
+  //
+  // Draw
+  function draw() {
+    isDrawingColor = false;
+    isDrawing = true;
+  }
+
+  // Color draw
+  function colorDraw() {
+    isDrawing = false;
+    isDrawingColor = true;
+  }
+
+  // Erase
+  function erase() {
+    isDrawing = false;
+    isDrawingColor = false;
+  }
+
+  // Clear function
+  function clear() {
+    cells.forEach((cell) => {
+      cell.classList.remove("color");
+      cell.style.backgroundColor = "";
+    });
+    isDrawing = false;
+    isDrawingColor = false;
+  }
 
   // Toggle grid borders
   function grid() {
@@ -44,38 +97,10 @@ function addListenersToCells() {
     });
   }
 
-  // Draw function
-  function draw() {
-    isDrawing = true;
-    cells.forEach((cell) => {
-      cell.addEventListener("mouseover", () => {
-        if (isDrawing) {
-          cell.classList.add("color");
-        }
-      });
-    });
-  }
-
-  // Erase function
-  function erase() {
-    isDrawing = false;
-    cells.forEach((cell) => {
-      cell.addEventListener("mouseover", () => {
-        cell.classList.remove("color");
-      });
-    });
-  }
-
-  function clear() {
-    isDrawing = false;
-    cells.forEach((cell) => {
-      cell.classList.remove("color");
-    });
-  }
-
   // Assign functions to buttons
   gridButton.addEventListener("click", grid);
   drawButton.addEventListener("click", draw);
+  colorButton.addEventListener("click", colorDraw);
   eraseButton.addEventListener("click", erase);
   clearButton.addEventListener("click", clear);
 }
@@ -83,12 +108,12 @@ function addListenersToCells() {
 // Listener for the size button to update grid size
 sizeButton.addEventListener("click", () => {
   size = Number(prompt("Grid size from 1 - 100"));
-  if (size >= 1 && size <= 100) {
+  if (size >= MIN_SIZE && size <= MAX_SIZE) {
     document.documentElement.style.setProperty("--size", size);
     resetGrid(); // Reset the old grid
     createGrid(size); // Create a new grid with the new size
   } else {
-    alert("Please enter a number between 1 and 100.");
+    alert(`Please enter a number between ${MIN_SIZE} and ${MAX_SIZE}.`);
   }
 });
 
